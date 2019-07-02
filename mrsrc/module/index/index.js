d8cf17269ab44c0bc1a22afd5b9db1f827d562e5
Mountain1210 <mountain_wang@126.com>
2019-07-02 17:15:02 +0800

    设置canvas的宽高

--- mrsrc/module/index/index.js
+++ mrsrc/module/index/index.js
@@ -13,17 +13,10 @@ document.getElementById('video').src = mov;
     //函数
    document.getElementById('btnCatchPicture').addEventListener('click', function(){
    	var video=document.getElementById("video");//获取id
-   	console.log(video.videoWidth)
-   	console.log("============================")
    	let canvas=document.getElementById("canvass");
-   	console.log("============================")
-   	// (canvas)
    	var ctx=canvas.getContext('2d');//类型是2d
    	canvas.width=video.videoWidth;//画布的宽度等于视频屏幕的宽度
    	canvas.height=video.videoHeight;
-   	ctx.drawImage(video,0,0,400,230);//在画布上画画
+   	ctx.drawImage(video,0,0,video.videoWidth,video.videoHeight);//在画布上画画
    	document.getElementById("win").style.display="block";
    })
-    // function CatchPictures(){
-      
-    // }
